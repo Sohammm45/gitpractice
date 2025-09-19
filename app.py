fe7db060f2227ac448b357dd3dec384e28fb8976
Soham Bhosale <sohambhosale407@gmail.com>
2025-09-19 10:41:47 +0530

    updated branch developer a code in main

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 def addition(a, b):
     return a + b
+def subtraction(a, b):
+    return a - b
+def multiplication(a, b):   
+    return a * b
+print("this is from my main branch");
+print("this is from my developer branch ")
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error! Division by zero."
